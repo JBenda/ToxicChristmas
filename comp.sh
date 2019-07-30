f06f193f4a8c57509ae99b763eab1f98e9b0bdb3
Julian Benda <julian.m.benda@gmail.com>
2019-07-30 15:47:51 +0200

    * moved wasm files in one Folder * updated comp script*

--- comp.sh
+++ comp.sh
@@ -1,1 +1,1 @@
-emcc test.c -o test.js -O3 -s WASM=1 -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=["ccall", "cwrap"]"
+emcc wasm/*.cpp -o wasmLoader.js -O3 -s WASM=1 -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=["cwrap"]" -std=c++11

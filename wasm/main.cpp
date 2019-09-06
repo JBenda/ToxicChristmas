052af4a748c621b3ae9732ea4d09784fc8570f1c
JBenda <Zabuza98@gmx.net>
2019-09-06 12:58:27 +0200

    Animation now handled from wasm

--- wasm/main.cpp
+++ wasm/main.cpp
@@ -1,22 +1,47 @@
 #include <emscripten.h>
-#include "shared.cpp"
+#include <iostream>
+#include <chrono>
+#include "shared.hpp"
 
-unsigned int frameCount = 0;
+
+unsigned int frameCount = 20;
+constexpr unsigned int amtFrame = 23;
+constexpr double frameTime = 0.03;
+double frameTimer = 0;
+
+bool first = false;
+std::chrono::time_point<std::chrono::system_clock> last{};
 
 int main() {
     shared::player_frameCount = &frameCount;
+    emscripten_run_script("BindASM();");
     return 0;
 }
 
-EMSCRIPTEN_KEEPALIVE
-unsigned int SharedPointerLength() {
-    return shared::amount;
-}
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE
+    unsigned int SharedPointerLength() {
+        return shared::amount;
+    }
 
-/**
- * @return array with construcdet: [length, pptr0, pptr1, pptr2, ...]
- */
-EMSCRIPTEN_KEEPALIVE
-void** GetSharedPointer(unsigned int id) {
-    return shared::shared[id];
-}
+    EMSCRIPTEN_KEEPALIVE
+    void*** GetSharedPointer() {
+        return shared::shared;
+    }
+
+    EMSCRIPTEN_KEEPALIVE
+    void Update() {
+        auto now = std::chrono::system_clock::now();
+        if(!first) {
+            std::chrono::duration<double> d = now - last;
+            double dT = d.count();
+            frameTimer += dT;
+            if (frameTimer > frameTime) {
+                frameTimer = 0;
+                if(++frameCount == amtFrame)
+                    frameCount = 0;
+            }
+        } else { first = false; }
+        last = now;
+    }
+};
--- wasm/shared.cpp
+++ wasm/shared.cpp
@@ -2,9 +2,14 @@
 
 namespace shared {
 
+const char str_player_frameCount[] = "player_frameCount";
+
 unsigned int *player_frameCount = nullptr;
 
 const unsigned int amount = 1;
-void **shared[] = {(void**)(&player_frameCount)};
+void **shared[] = {
+    (void**)str_player_frameCount,
+    (void**)&player_frameCount
+};
 
 };
